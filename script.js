f7834fbc251de3a6aca3131279b0f6ac1e4b4d82
DRD667 <68093275+DRD667@users.noreply.github.com>
2021-11-03 13:31:50 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,13 @@
+function showMenu(){
+    
+    const menu = document.getElementById('nav1');
+    
+    if(menu.style.display == "none"){
+        menu.style.display = "flex";
+        
+    } 
+    else{
+        menu.style.display = "none";
+        
+    }
+}
